2ba8b5ed1886d1f7a1352d1b8fb0ea5d36928b7c
Long On <on.long.on@goomail.fake>
2013-10-02 01:29:21 -0400

    Add the API Key Client

--- lib/api_key_client.rb
+++ lib/api_key_client.rb
@@ -1,10 +1,55 @@
 module RubyMeetup
 
-class ApiKeyClient < Client
+# This class is typically used to read data without needing to first obtain a
+# user authenticated +access_token+. We just need a valid API Key, which any one
+# can obtain directly from http://www.meetup.com
+#
+# The API key can be configured globally like so,
+#
+#  > RubyMeetup::ApiKeyClient.key = 'abcd000000000000000wxyz'
+#
+# Then create a new client and make a request,
+#
+#  > client = RubyMeetup::ApiKeyClient.new
+#  > json_string = client.get_path("/2/groups", {:group_id => 390230})
+# 
+# The same client instance may be used to make multiple requests. Just supply a
+# different @path and/or @options parameters as required.
+#
+# ---
+# Copyright (c) 2013 Long On, released under the MIT license
 
-  def hello
-    "Hello API World"
+class ApiKeyClient < Client
+  @@key = ""
+
+  # == class methods
+
+  # Set the global API Key
+  def self.key=string
+    @@key = string
   end
+
+  # == instance methods
+
+  # :nodoc:
+  def to_s
+    s = super
+    s << ", key=#{short_key}"
+  end
+
+
+  protected
+    # :nodoc:
+    def merge_params(options)
+      {:sign => true, :key => @@key}.merge!(options)
+    end
+
+    # :nodoc:
+    def short_key
+      key = @@key
+      return "" if key.nil? || key.empty?
+      key[0..3] + "..." + key[(key.length - 4)..(key.length - 1)]
+    end
 
 end
 
--- test/test_key_client.rb
+++ test/test_key_client.rb
@@ -3,9 +3,14 @@ require 'ruby_meetup'
 
 class TestKeyClient < Test::Unit::TestCase
 
-  def test_hello
+  def test_printing
+    s = "RubyMeetup::ApiKeyClient: site=https://api.meetup.com, path="
     client = RubyMeetup::ApiKeyClient.new
-    assert_equal "Hello API World", client.hello
+    assert_equal (s + ", key="), client.to_s
+    client.path = "/2/groups"
+    assert_equal (s + "/2/groups, key="), client.to_s
+    client.class.key = 'abcd000000000007890'
+    assert_equal (s + "/2/groups, key=abcd...7890"), client.to_s
   end
 
 end
